816f95dfa49761d401dcfa12e185af2af6aebda5
Évelim Dornelles <dorrrnelles@gmail.com>
2024-11-15 14:58:30 -0300

    upload

---
+++ engine.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+    // Adiciona a classe 'active' assim que a página carregar
+    document.querySelector('.box-img').classList.add('active');
+    document.querySelector('.box-titulo').classList.add('active');
+};
